0f24f605641bd1c24bcf5d60b8f95fb977468418
nurlan-dev-tiul <tulkubaev.nurlan@gmail.com>
2022-07-01 21:29:01 +0600

    cors-fix

--- server.js
+++ server.js
@@ -13,7 +13,23 @@ const userRoutes = require('./routes/usersRoutes/users');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
+    }
+        next()
+})
 
 
 //! Mongo DB Connection
