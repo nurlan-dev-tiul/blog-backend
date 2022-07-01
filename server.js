35c8e1a7d9f0ab64eb1f570654ea9e15e0a2cf13
nurlan-dev-tiul <tulkubaev.nurlan@gmail.com>
2022-07-02 00:02:43 +0600

    cors-fix

--- server.js
+++ server.js
@@ -16,6 +16,23 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+    if(req.rawBody === undefined && req.method === 'POST' && req.headers['content-type'].startsWith('multipart/form-data')){
+        getRawBody(req, {
+            length: req.headers['content-length'],
+            limit: '10mb',
+            encoding: contentType.parse(req).parameters.charset
+        }, function(err, string){
+            if (err) return next(err)
+            req.rawBody = string
+            next()
+        })
+    } else {
+        next()
+    }
+})
+
+
 
 //! Mongo DB Connection
 dbConnect();
